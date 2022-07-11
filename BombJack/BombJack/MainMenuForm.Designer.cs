546d1103a69263965bf00706ffab67b6cc4e4162
hrivnad <david.hrivna@seznam.cz>
2022-07-11 11:31:38 +0200

    Init

---
+++ BombJack/BombJack/MainMenuForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace BombJack
+{
+    partial class MainMenuForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_btn = new System.Windows.Forms.Button();
+            this.controls_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(12, 12);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(98, 23);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start Game";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // controls_btn
+            // 
+            this.controls_btn.Location = new System.Drawing.Point(12, 41);
+            this.controls_btn.Name = "controls_btn";
+            this.controls_btn.Size = new System.Drawing.Size(98, 23);
+            this.controls_btn.TabIndex = 1;
+            this.controls_btn.Text = "Show Controls";
+            this.controls_btn.UseVisualStyleBackColor = true;
+            // 
+            // MainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.controls_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "MainMenuForm";
+            this.Text = "Bomb Jack - Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button start_btn;
+        private Button controls_btn;
+    }
+}
---
+++ BombJack/BombJack/MainMenuForm.cs
@@ -0,0 +1,27 @@
+namespace BombJack
+{
+    public partial class MainMenuForm : Form
+    {
+        public MainMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            MainGameForm game = new MainGameForm();
+            this.Hide();
+            switch (game.ShowDialog())
+            {
+                case DialogResult.TryAgain: // New game
+                    //game = new MainGameForm();
+                    break;
+                case DialogResult.Continue: // Back to menu
+
+                    break;
+                default: // Error
+                    break;
+            }
+        }
+    }
+}
